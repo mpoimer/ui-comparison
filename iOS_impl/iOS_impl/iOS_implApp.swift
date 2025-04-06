337723adcab522dcb80d88447553ab905705ad49
Mathias Poimer <poimerm1@gmail.com>
2025-04-06 12:42:02 +0200

    #4 iOS impl

---
+++ iOS_impl/iOS_impl/iOS_implApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_implApp.swift
+//  iOS_impl
+//
+//  Created by Mathias Poimer on 09.03.25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_implApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
